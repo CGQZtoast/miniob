c376bbca868637fb74329164524c0b36655722fb
toast <212133061@qq.com>
2024-03-16 15:19:33 +0800

    修复删除索引后再次创建相同索引时失败问题

--- src/observer/sql/stmt/drop_index_stmt.cpp
+++ src/observer/sql/stmt/drop_index_stmt.cpp
@@ -7,15 +7,15 @@
 using namespace std;
 using namespace common;
 
-RC DropIndexStmt::create(Db *db, const DropIndexSqlNode &create_index, Stmt *&stmt)
+RC DropIndexStmt::create(Db *db, const DropIndexSqlNode &drop_index, Stmt *&stmt)
 {
   stmt = nullptr;
 
-  const char *table_name = create_index.relation_name.c_str();
-  if (is_blank(table_name) || is_blank(create_index.index_name.c_str()) ||
-      is_blank(create_index.relation_name.c_str())) {
+  const char *table_name = drop_index.relation_name.c_str();
+  if (is_blank(table_name) || is_blank(drop_index.index_name.c_str()) ||
+      is_blank(drop_index.relation_name.c_str())) {
     LOG_WARN("invalid argument. db=%p, table_name=%p, index name=%s, relation name=%s",
-        db, table_name, create_index.index_name.c_str(), create_index.relation_name.c_str());
+        db, table_name, drop_index.index_name.c_str(), drop_index.relation_name.c_str());
     return RC::INVALID_ARGUMENT;
   }
 
@@ -26,12 +26,12 @@ RC DropIndexStmt::create(Db *db, const DropIndexSqlNode &create_index, Stmt *&st
     return RC::SCHEMA_TABLE_NOT_EXIST;
   }
 
-  Index *index = table->find_index(create_index.index_name.c_str());
+  Index *index = table->find_index(drop_index.index_name.c_str());
   if (nullptr == index) {
-    LOG_WARN("index with name(%s) not exists. table name=%s", create_index.index_name.c_str(), table_name);
+    LOG_WARN("index with name(%s) not exists. table name=%s", drop_index.index_name.c_str(), table_name);
     return RC::SCHEMA_INDEX_NAME_REPEAT;
   }
 
-  stmt = new DropIndexStmt(table, create_index.index_name);
+  stmt = new DropIndexStmt(table, drop_index.index_name);
   return RC::SUCCESS;
 }
